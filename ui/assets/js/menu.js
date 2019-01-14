032b8b1fcbb47415854c3bbb969718aeb7ba8e46
yungtravla <lilprismontour@gmail.com>
2019-01-14 18:14:12 +1000

    improve ui

--- ui/assets/js/menu.js
+++ ui/assets/js/menu.js
@@ -4,24 +4,20 @@
 	// Reveal submenus on desktop and mobile
 	document.querySelectorAll("html body div.menu div.item").forEach(async(item)=>{
 		if (item.getAttribute("name") !== "xssbutton") {
-			item.addEventListener("mouseenter", async(event)=>{
+			item.addEventListener("pointerenter", async(event)=>{
 				event.target.setAttribute("data-state", "on");
+			});
 
-				item.addEventListener("mouseleave", async function a(event){
+			item.addEventListener("pointerleave", async(event)=>{
+				event.target.setAttribute("data-state", "off");
+			});
+
+			item.addEventListener("click", async(event)=>{
+				if ( event.target.getAttribute("data-state") == "off" ) {
+					event.target.setAttribute("data-state", "on");
+				} else {
 					event.target.setAttribute("data-state", "off");
-					this.removeEventListener("mouseleave", a);
-				});
-
-				event.target.addEventListener("click", async function b(event){
-					if ( event.target.getAttribute("data-state") == "off" ) {
-						event.target.setAttribute("data-state", "on");
-						this.removeEventListener("click", b);
-						this.removeEventListener("click", b);
-					} else {
-						event.target.setAttribute("data-state", "off");
-						this.removeEventListener("click", b);
-					}
-				});
+				}
 			});
 		}
 	});
