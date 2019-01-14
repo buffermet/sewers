9ca5d16a1f8b5b65f8f7ac9d18e8c4fa3dc90daf
yungtravla <lilprismontour@gmail.com>
2019-01-14 18:05:15 +1000

    improve ui

--- ui/assets/js/menu.js
+++ ui/assets/js/menu.js
@@ -6,16 +6,19 @@
 		if (item.getAttribute("name") !== "xssbutton") {
 			item.addEventListener("mouseenter", async(event)=>{
 				event.target.setAttribute("data-state", "on");
+
+				event.target.addEventListener("click", async function f(event){
+					if ( event.target.getAttribute("data-state") == "off" ) {
+						event.target.setAttribute("data-state", "on");
+						this.removeEventListener("click", f);
+					} else {
+						event.target.setAttribute("data-state", "off");
+						this.removeEventListener("click", f);
+					}
+				});
 			});
 			item.addEventListener("mouseleave", async(event)=>{
 				event.target.setAttribute("data-state", "off");
-			});
-			item.addEventListener("mouseup", async(event)=>{
-				if ( event.target.getAttribute("data-state") == "off" ) {
-					event.target.setAttribute("data-state", "on");
-				} else {
-					event.target.setAttribute("data-state", "off");
-				}
 			});
 		}
 	});
