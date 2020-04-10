1b7af69741bb347d3cf063bb40faae1939b37b33
yungtravla <lilprismontour@gmail.com>
2020-04-10 22:15:09 +1000

    update

--- core/obfuscation/obfuscation.go
+++ core/obfuscation/obfuscation.go
@@ -7,16 +7,19 @@ package obfuscation
 */
 
 import (
+  "errors"
   "math/rand"
   "regexp"
+  "strconv"
   "time"
 )
 
-type codeSet struct {
+type CodeSet struct {
   AfterSets  []int
   BeforeSets []int
   ID         int
   Imports    map[int]string
+  NestedSets []int
   Variants   []string
 }
 
@@ -108,26 +111,136 @@ func ObfuscateSource(payload []byte, wordlist []string, shuffle bool) ([]byte, e
   return payload, nil
 }
 
-  // AfterSets  []int
-  // BeforeSets []int
-  // CodeSets   []int
-  // ID         int
-  // Imports    map[int]string
-  // Variants   []string
+func ShuffleSource(codeSets []*CodeSet) ([]byte, error) {
+  for a := 0; a < len(codeSets); a++ {
+    this_set := codeSets[a]
 
-func ShuffleSource(codeSets []*codeSet) ([]byte, error) {
+    rand.Seed(time.Now().UnixNano())
+    index := rand.Intn(len(this_set.NestedSets) + 1)
+    nested_code := this_set.NestedSets[index]
 
+    return nil, errors.New("code set " + strconv.Itoa(this_set.ID) + " is missing the following nested set of code: " + strconv.Itoa(this_nested_code.ID))
+  }
 
   return nil, nil
 }
 
+// func RandomString(length int) string {
+//   rand.Seed(time.Now().UnixNano())
+//   chars := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+//   buffer := ""
+//   for i := 0; i < length; i++ {
+//     index := rand.Intn(length)
+//     buffer += string(chars[index])
+//   }
+//   return buffer
+// }
+
 /*
+
+[]
+[]
+i
+[]
+[]
+[""]
+
+[]
+[]
+i
+[]
+[]
+[""]
+
+[]
+[]
+i
+[]
+[]
+[""]
+
+[]
+[]
+i
+[]
+[]
+[""]
+
+[]
+[]
+i
+[]
+[]
+[""]
+
+[]
+[]
+i
+[]
+[]
+[""]
+
+[2,3,4,5,6,7]
+[]
+8
+[]
+[]
+["return buffer;"]
+
+[]
+[8]
+7
+[]
+[]
+["buffer += string(chars[index]);"]
+
+[]
+[8]
+6
+["math/rand"]
+[]
+["index := rand.Intn(length);","index := int(rand.Float64() * length);","index := int(length * rand.Float64());"]
+
+[]
+[8]
+5
+[]
+[6,7]
+["for i := 0; i < length; i++ {\n{{NESTED_CODE}}\n};"]
+
+[2,3]
+[2,3,8]
+4
+[]
+[]
+["buffer := \"\";"]
+
+[2,4]
+[2,4,8]
+3
+[]
+[]
+["chars := []byte(\"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\");"]
+
+[3,4]
+[3,4,8]
+2
+["math/rand","time"]
+[]
+["rand.Seed(time.Now().UnixNano());"]
 
 [0]
 [0]
-[]
 1
+["math/rand","time"]
+[2,3,4]
+["func RandomString(length int) string {\n{{NESTED_CODE}}\n};"]
+
+[1]
+[1]
+0
 ["errors"]
-["func ShuffleSource(codeSets []codeSet) ([]byte, error) {\n{{NESTED_CODE}}\n}"]
+[]
+["func ShuffleSource(codeSets []codeSet) ([]byte, error) {\n{{NESTED_CODE}}\n};"]
 
 */
